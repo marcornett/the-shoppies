bcf0af858467c58e6f99a79f5f2a6c2becd4c1d7
Marcel Cornett <marcornett@gmail.com>
2021-01-03 19:08:45 -0500

    State provider wrapped around entire app.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {StateProvider} from './ContextAPI/StateProvider'
+import reducer, {intialState} from './ContextAPI/reducer'
 
 ReactDOM.render(
   <React.StrictMode>
+    <StateProvider intialState={intialState} reducer={reducer}>
     <App />
+    </StateProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
