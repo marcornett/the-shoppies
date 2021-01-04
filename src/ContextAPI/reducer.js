8a29dc6b4e042bb162c1e56b53498b5d86eb7c62
Marcel Cornett <marcornett@gmail.com>
2021-01-03 20:24:54 -0500

    New case for adding nominees to global state.

--- src/ContextAPI/reducer.js
+++ src/ContextAPI/reducer.js
@@ -1,17 +1,24 @@
 export const ADD_MOVIE_DATA = 'ADD_MOVIE_DATA'
+export const ADD_NOMINEE = 'ADD_NOMINEE'
 export const intialState = {
     movieData: {},
-    searchTerm: ''
+    searchTerm: '',
+    nominees: [],
 }
 
 export default function reducer(state = intialState, action){
     switch (action.type){
-        case "ADD_MOVIE_DATA":
+        case ADD_MOVIE_DATA:
             return{
                 ...state,
                 movieData: action.payload,
                 searchTerm: action.searchTerm
             }
+        case ADD_NOMINEE:
+                return{
+                    ...state,
+                    nominees: [...state.nominees, action.payload]
+                }
         default:
             return state
     }
