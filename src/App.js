5e994e84fe8c6e8e0d0321aa3234e06f1a60271a
Marcel Cornett <marcornett@gmail.com>
2021-01-04 22:08:58 -0500

    Rendering nominations component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import SearchBar from './Components/SearchBar/SearchBar'
 import MovieResults from './Components/MovieResults/MovieResults'
+import Nominations from './Components/Nominations/Nominations'
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <SearchBar />
       <MovieResults />
+      <Nominations />
     </div>
   );
 }
